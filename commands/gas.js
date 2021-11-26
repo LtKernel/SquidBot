2aab8bd4446f25f98f7828d7eb048590fb7990af
LT-Kernel <kernel.ps2@gmail.com>
2021-11-26 15:52:14 -0800

    More table experiments.

--- commands/gas.js
+++ commands/gas.js
@@ -3,10 +3,11 @@ const { MessageEmbed } = require('discord.js');
 const { AsciiTable3 } = require('ascii-table3');
 
 const gasTable = new AsciiTable3()
-	.setHeading('', 'Name', 'Age')
-	.addRow(1, 'Bob', 52)
-	.addRow(2, 'John', 34)
-	.addRow(3, 'Jim', 83)
+	.setHeading('', 'Base',         'Generator', 'Coords', 'Tek', 'Main', 'Standby', 'Elem','Shards', 'Radius', 'Tek/hr', 'Gas', 'Obs Data',   'Rem Shards', 'Rem Gas')
+	.addRow(1,      'Tree of Life', 'Central',   '52/32',  'Yes',  'No',  'No',      '53',  '1000',   '10',    '3.7',     'NA' ,  '11/26/2021', '100',        'NA'     )
+	.addRow(1, 'Tree of Life', 52)
+	.addRow(2, 'Foundtain of Life', 34)
+	.addRow(2, 'Foundtain of Life', 34)
 	.removeBorder();
 
 // inside a command, event listener, etc.
@@ -16,15 +17,15 @@ const exampleEmbed = new MessageEmbed()
 	// .setURL('https://discord.js.org/')  <-- will link to a gas webpage someday
 	.setDescription('Projects how much longer a generator will remain online based on the amount of fuel last observed in it.')
 	.setThumbnail('https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/6/6e/Gasoline.png/revision/latest?cb=20150615125445')
-	.addField('Table', '`' + gasTable.toString() + '`', false)
 	.setTimestamp()
 	.setFooter('Last refreshed:', 'https://i.imgur.com/AfFp7pu.png');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('gasstats')
-		.setDescription('Loads the gasstats.'),
+		.setName('gas')
+		.setDescription('Loads the current fuel levels.'),
 	async execute(interaction) {
-		await interaction.reply({ embeds: [exampleEmbed] });
+		// const message = await interaction.reply({ embeds: [exampleEmbed], fetchReply: true });
+		await message.react('`' + gasTable.toString() + '`');
 	},
 };
