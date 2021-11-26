475754b7f6e4f5b112844f15baa61ec5ca7d913d
LT-Kernel <kernel.ps2@gmail.com>
2021-11-26 12:40:31 -0800

    First table print out.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # SquidBot
 A Discord bot for the game ARK: Survival Evolved
 
+# Install ESLint package (error checking)
+npm install --save-dev eslint
+# https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+
 # initialize the node project directory
 npm init
 
@@ -12,6 +16,9 @@ npm install @discordjs/builders @discordjs/rest discord-api-types
 
 # Install utilitiy method to build slash commands
 npm install @discordjs/builders
+
+# install utility to print ASCII tables
+npm install ascii-table3
 
 # Perms
 # Read Messages
--- commands/gasstats.js
+++ commands/gasstats.js
@@ -1,23 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const Discord = require('discord.js');
-const { MessageAttachment, MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
+const { AsciiTable3 } = require('ascii-table3');
+
+const gasTable = new AsciiTable3()
+	.setHeading('', 'Name', 'Age')
+	.addRow(1, 'Bob', 52)
+	.addRow(2, 'John', 34)
+	.addRow(3, 'Jim', 83)
+	.removeBorder();
 
 // inside a command, event listener, etc.
-const exampleEmbed = new Discord.MessageEmbed()
+const exampleEmbed = new MessageEmbed()
 	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Regular field title', 'Some value here')
-	//.addBlankField()
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTitle('Generator Fuel Status')
+	// .setURL('https://discord.js.org/')  <-- will link to a gas webpage someday
+	.setDescription('Projects how much longer a generator will remain online based on the amount of fuel last observed in it.')
+	.setThumbnail('https://static.wikia.nocookie.net/arksurvivalevolved_gamepedia/images/6/6e/Gasoline.png/revision/latest?cb=20150615125445')
+	.addField('Table', '`' + gasTable.toString() + '`', false)
 	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+	.setFooter('Last refreshed:', 'https://i.imgur.com/AfFp7pu.png');
 
 module.exports = {
 	data: new SlashCommandBuilder()
