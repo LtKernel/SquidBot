188f2f33ab0e16c2efdb7a94c05784d89f557da1
LT-Kernel <kernel.ps2@gmail.com>
2022-10-13 16:40:20 -0700

    Implemented print by library name.

--- commands/stats.js
+++ commands/stats.js
@@ -1,41 +1,82 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { AsciiTable3 } = require('ascii-table3');
+const { ClientInvalidOption } = require('discord.js/src/errors/ErrorCodes');
 // const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 // Load the stats database
-const stats = require('../stats.json');
+const statsJson = require('../stats.json');
 
-const statsTable = new AsciiTable3();
+// Mobile-friendly output
+const compact = flase;
 
-stats.Database.Libraries.forEach(function(library) {
-	// if(library.Name == ARG)
-	statsTable.setHeading('Dino', 'Server', 'Owner', 'Sex', 'Health', 'Stamina', 'Oxygen', 'Food', 'Weight', 'Melee', 'Speed');
-	library.Dinos.forEach(function(dino) {
-		statsTable.addRow(dino.Creature,
-			dino.Server,
-			dino.Owner,
-			dino.Sex,
-			dino.Health,
-			dino.Stamina,
-			dino.Oxygen,
-			dino.Food,
-			dino.Weight,
-			dino.Melee,
-			dino.Speed);
-	});
+function compactServer(server) {
+	return {
+		'The Island': 'TI',
+		'Scorched Earth': 'SE',
+		'Abberation': 'AB',
+		'Extinction': 'EX',
+		'Genesis: Part 1': 'G1',
+		'Genesis: Part 2': 'G2',
+		'The Center': 'TC',
+		'Ragnarok': 'RA',
+		'Valguaro': 'VA',
+		'Crystal Isles': 'CI',
+		'Los Island': 'LI',
+		default: '??',
+	}[server];
+}
+
+function compactSex(sex) {
+	return {
+		'Male': 'M',
+		'Female': 'F',
+		default: '?',
+	}[sex];
+}
+
+function getLibrary(name) {
+
+	const statsTable = new AsciiTable3();
 	statsTable.removeBorder();
-	console.log(statsTable.toString());
-});
 
+	if (compact) {
+		statsTable.setHeading('Creature', 'Srv', 'Owner', 'Ge', 'HP', 'St', 'Ox', 'Fo', 'We', 'Dm', 'Sp');
+	}
+	else {
+		statsTable.setHeading('Creature', 'Server', 'Owner', 'Sex', 'Health', 'Stamina', 'Oxygen', 'Food', 'Weight', 'Melee', 'Speed');
+	}
+
+	statsJson.Database.Libraries.forEach((library) => {
+		if (library.Name == name) {
+			library.Dinos.forEach(function(dino) {
+				statsTable.addRow(
+					dino.Creature,
+					compactServer(dino.Server),
+					dino.Owner,
+					compactSex(dino.Sex),
+					dino.Health,
+					dino.Stamina,
+					dino.Oxygen,
+					dino.Food,
+					dino.Weight,
+					dino.Melee,
+					dino.Speed);
+			});
+		}
+	});
+
+	return statsTable.rows.length > 0 ? statsTable.toString() : `Could not find a libary named ${name}`;
+}
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('dinostats')
+		.setName('stats')
 		.setDescription('Loads the current breeding stats.')
 		.addStringOption(option =>
-			option.setName('input')
-				.setDescription('The input to echo back')
+			option.setName('library')
+				.setDescription('The stats library to accesss. Enter /"List/" to see available libraries')
 				.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Last updated: Friday, November 26, 2021 4:20 PM' + '\r\n' + '`' + statsTable + '`' });
+		const libraryName = interaction.options.getString('library');
+		await interaction.reply({ content: 'Last updated: Friday, November 26, 2021 4:20 PM' + '\r\n' + '`' + getLibrary(libraryName) + '`' });
 	},
 };
