27b2d310de977ce8781569f35d7cf9a362a721a2
LT-Kernel <kernel.ps2@gmail.com>
2022-10-18 21:56:29 -0700

    Switched to single quotes.

--- src/bot.js
+++ src/bot.js
@@ -1,15 +1,11 @@
-require("dotenv").config();
+require('dotenv').config();
+const fs = require('fs');
 const { token, database } = process.env;
-const fs = require("fs");
-const { connect } = require("mongoose");
+const { connect } = require('mongoose');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
-// Require the necessary discord.js classes
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
-// const { MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
-
-// load config variables
-// Warni9ng: do not checkin config.json with actual token code included
-const config = require("../config.json");
+// Load config variables
+// const config = require('../config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -18,26 +14,26 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 client.commandArray = [];
 
-// grab all the handlers from the functions folders to handle all
-// commands and events and pass the client into each.
-const functionFolders = fs.readdirSync("./src/functions");
+// Grab all the handlers from the functions folders to handle
+// all commands and events and pass the client into each.
+const functionFolders = fs.readdirSync('./src/functions');
 for (const folder of functionFolders) {
   const functionFiles = fs
     .readdirSync(`./src/functions/${folder}`)
-    .filter((file) => file.endsWith(".js"));
+    .filter((file) => file.endsWith('.js'));
   for (const file of functionFiles) {
     require(`./functions/${folder}/${file}`)(client);
   }
 }
 
-// Call the handler functions
+// Call the handler functions.
 client.handleCommands();
 client.handleEvents();
 
-// Finally log in to Discord with API token credentials
+// Finally log in to Discord with API token credentials.
 client.login(token);
 
-// Connect to the mongo database with database connect string
+// Connect to the mongo database with database connect string.
 (async () => {
   await connect(database).catch(console.error);
 })();
