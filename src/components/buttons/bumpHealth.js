4d0ceca2de5c6028649c6cc478081994db65c069
LT-Kernel <kernel.ps2@gmail.com>
2022-10-19 00:19:51 -0700

    Working on Health button.

--- src/components/buttons/bumpHealth.js
+++ src/components/buttons/bumpHealth.js
@@ -1,10 +1,21 @@
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
   data: {
     name: 'bumpHealth',
   },
   async execute(interaction, client) {
-    await interaction.reply({
-      content: '[Buttons] Bumping health',
+
+    // Get the current value of the menu
+    // interaction.values[0];
+
+    // Update the database value
+
+    // Reprint the table
+
+    await interaction.update({
+      content: 'test',
+
     });
   },
 };
